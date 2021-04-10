acef2c82b978e378a5efae3b42c850a78aeda726
jack <jackjack5810@yahoo.com>
2021-04-10 09:31:35 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const bundleOutputDir = "./dist";
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: "./src/main"  
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.join(__dirname, bundleOutputDir),
+        publicPath: 'public/dist/'
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ['/node_modules/']
+            },            
+            { test: /\.tsx?$/, loader: "ts-loader" },        
+            {
+                test: /\.css$/,
+                sideEffects: true,
+                loader: "css-loader"
+            }
+        ]
+    },
+    plugins:[
+        new CleanWebpackPlugin()
+    ]
+};
+
